24b52b66f0c877367c2750aef6b9c9af95dd4b0a
paulstaker <paulstaker@pm.me>
2022-04-11 13:16:46 -0400

    operator address

--- migrations/4_deploy_eth_sacred.js
+++ migrations/4_deploy_eth_sacred.js
@@ -7,7 +7,7 @@ const hasherContract = artifacts.require('Hasher')
 
 module.exports = function(deployer, network, accounts) {
   return deployer.then(async () => {
-    const { MERKLE_TREE_HEIGHT, ETH_AMOUNTS, OPERATOR_FEE } = process.env
+    const { MERKLE_TREE_HEIGHT, ETH_AMOUNTS, OPERATOR_ADDRESS, OPERATOR_FEE } = process.env
     const ethAmounts = ETH_AMOUNTS.split(",");
     const verifier = await Verifier.deployed()
     const hasherInstance = await hasherContract.deployed()
@@ -15,7 +15,7 @@ module.exports = function(deployer, network, accounts) {
       let amount = ethAmounts[i];
       console.log("Deploying ETHSacred instance: ", amount)
       await ETHSacred.link(hasherContract, hasherInstance.address)
-      const sacred = await deployer.deploy(ETHSacred, verifier.address, amount, MERKLE_TREE_HEIGHT, accounts[0], OPERATOR_FEE)
+      const sacred = await deployer.deploy(ETHSacred, verifier.address, amount, MERKLE_TREE_HEIGHT, OPERATOR_ADDRESS, OPERATOR_FEE)
       console.log('ETHSacred\'s address ', sacred.address)
     }
     
